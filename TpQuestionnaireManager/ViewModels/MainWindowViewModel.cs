c896a6f67173705e7b0e23f03a081a80336efbce
dante18 <lablandinerie2@gmail.com>
2025-09-17 17:04:29 +0200

    Feat: Update ViewModels link to MainWindows and MainWindows

--- TpQuestionnaireManager/ViewModels/MainWindowViewModel.cs
+++ TpQuestionnaireManager/ViewModels/MainWindowViewModel.cs
@@ -1,27 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
-using TpQuestionnaireManager.Data.AccessLayers;
 using TpQuestionnaireManager.Data.Models;
+using TpQuestionnaireManager.Services;
 
 namespace TpQuestionnaireManager.ViewModels;
 
 class MainWindowViewModel : ObservableObject
 {
-    private readonly QuestionnaireRepository _questionnaireRepository;
+    private readonly QuestionnaireService questionnaireService;
+
     public ObservableCollection<Questionnaire> Questionnaires { get; } = new();
 
     public MainWindowViewModel() { 
-        _questionnaireRepository = new QuestionnaireRepository();
-        LoadData();
-    }
-
-    private void LoadData()
-    {
-        var questionnaires = _questionnaireRepository.GetAllQuestionnaires();
-        foreach (var q in questionnaires)
-        {
-            Questionnaires.Add(q);
-        }
+        this.questionnaireService = new QuestionnaireService();
+        this.Questionnaires = this.questionnaireService.GetAllQuestionnaires();
     }
 }
