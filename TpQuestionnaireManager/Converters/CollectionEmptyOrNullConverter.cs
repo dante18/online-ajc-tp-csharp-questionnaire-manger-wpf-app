b182c5c627d4458f2875dadebcdf77016e8db1c8
Thomas NACHAR <thomas.nachar18@gmail.com>
2025-09-18 14:31:51 +0200

    Converter pour collection nulle ou vide

---
+++ TpQuestionnaireManager/Converters/CollectionEmptyOrNullConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TpQuestionnaireManager.Converters;
+
+public class CollectionEmptyOrNullConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var collection = value as ICollection;
+        if (collection == null || collection.Count == 0)
+            return Visibility.Visible;
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- TpQuestionnaireManager/Converters/Converter.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace TpQuestionnaireManager.Converters;
-
-class Converter
-{
-}
