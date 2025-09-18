8690e92605c314101794df9af38cb17e9b6a49ae
dante18 <lablandinerie2@gmail.com>
2025-09-18 12:52:16 +0200

    Feat: Add methods used to add new questionnaire

--- TpQuestionnaireManager/ViewModels/ListeQuestionnaireViewModel.cs
+++ TpQuestionnaireManager/ViewModels/ListeQuestionnaireViewModel.cs
@@ -3,34 +3,70 @@ using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
-using TpQuestionnaireManager.Data.AccessLayers;
 using TpQuestionnaireManager.Data.Models;
 using TpQuestionnaireManager.Services;
 using TpQuestionnaireManager.Views;
 
 namespace TpQuestionnaireManager.ViewModels;
 
-class ListeQuestionnaireViewModel : ObservableObject
-    {
+[ObservableObject]
+public sealed partial class ListeQuestionnaireViewModel
+{
     private readonly QuestionnaireService questionnaireService;
 
-    public ICommand DeleteCommand { get; set; } = null!;
-    public ICommand ModifierCommand { get; } = null!;
+    public ICommand AjouterCommand { get; set; } = null!;
+
+    public ICommand SupprimerCommand { get; set; } = null!;
+
+    public ICommand ModifierCommand { get; set; } = null!;
 
     public ObservableCollection<Questionnaire> Questionnaires { get; } = new();
+
+    [ObservableProperty]
+    private string? nouveauQuestionnaireTitre;
 
     public ListeQuestionnaireViewModel()
     {
         this.questionnaireService = new QuestionnaireService();
         this.Questionnaires = this.questionnaireService.GetAllQuestionnaires();
 
-        ModifierCommand = new RelayCommand<Questionnaire>(ModifierQuestionnaire);
+        this.AjouterCommand = new RelayCommand(this.AjouterQuestionnaire);
+        this.ModifierCommand = new RelayCommand<Questionnaire>(this.ModifierQuestionnaire);
+        this.SupprimerCommand = new RelayCommand<Questionnaire>(this.SupprimerQuestionnaire);
     }
 
-    private void ModifierQuestionnaire(Questionnaire q)
+    private void AjouterQuestionnaire()
     {
+        if (string.IsNullOrWhiteSpace(this.nouveauQuestionnaireTitre))
+            return;
+
         var mainWindow = Application.Current.MainWindow as MainWindow;
-        mainWindow?.MainFrame.Navigate(new DetailQuestionnaire(q));
+
+        var questionnaire = new Questionnaire { Titre = this.nouveauQuestionnaireTitre };
+        questionnaireService.AddQuestionnaire(questionnaire);
+        Questionnaires.Add(questionnaire);
+
+        this.nouveauQuestionnaireTitre = string.Empty;
+
+        mainWindow?.MainFrame.Navigate(new DetailQuestionnaire(questionnaire));
+    }
+
+    private void ModifierQuestionnaire(Questionnaire questionnaire)
+    {
+        if (questionnaire is not null)
+        {
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow?.MainFrame.Navigate(new DetailQuestionnaire(questionnaire));
+        }
+    }
+
+    private void SupprimerQuestionnaire(Questionnaire? questionnaire)
+    {
+        if (questionnaire is not null)
+        {
+            this.questionnaireService.RemoveQuestionnaire(questionnaire);
+            this.Questionnaires.Remove(questionnaire);
+        }
     }
 }
 
